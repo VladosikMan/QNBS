1168dd07dce0252e4567b0f0325f441c1881cdc1
Vladislav Vedernikov <vladved2424@gmail.com>
2022-06-06 13:53:38 +0300

    separate code camera in separate fragment

---
+++ app/src/main/java/com/vladgad/qnb/CameraFragment.kt
@@ -0,0 +1,58 @@
+package com.vladgad.qnb
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Size
+import android.view.View
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.android.synthetic.main.frag_qr_scanner.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CameraFragment : Fragment(R.layout.frag_qr_scanner) {
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var analyzer: MyImageAnalyzer
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        analyzer = MyImageAnalyzer(getParentFragmentManager())
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        cameraProviderFuture = activity?.let { ProcessCameraProvider.getInstance(it.applicationContext) } as ListenableFuture<ProcessCameraProvider>
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(activity!!.applicationContext))
+    }
+
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview: Preview = Preview.Builder()
+            .build()
+        val cameraSelector: CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+        preview.setSurfaceProvider(previewView.createSurfaceProvider(null))
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280, 720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+        imageAnalysis.setAnalyzer(cameraExecutor, analyzer)
+
+        cameraProvider.bindToLifecycle(
+            this as LifecycleOwner,
+            cameraSelector,
+            imageAnalysis,
+            preview
+        )
+    }
+}
--- app/src/main/java/com/vladgad/qnb/MainActivity.kt
+++ app/src/main/java/com/vladgad/qnb/MainActivity.kt
@@ -4,13 +4,18 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Insets.add
 import android.net.Uri
+import android.net.sip.SipManager.newInstance
+import android.net.wifi.p2p.nsd.WifiP2pUpnpServiceRequest.newInstance
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.util.Size
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
@@ -18,21 +23,41 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.google.android.gms.common.ErrorDialogFragment.newInstance
+import com.google.android.material.datepicker.MaterialCalendar.newInstance
 import com.google.common.util.concurrent.ListenableFuture
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import kotlinx.android.synthetic.main.activity_main.*
+import org.xmlpull.v1.XmlPullParserFactory.newInstance
+import java.lang.reflect.Array.newInstance
+import java.net.URLClassLoader.newInstance
+import javax.xml.datatype.DatatypeFactory.newInstance
 
 class MainActivity : AppCompatActivity() {
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var cameraExecutor: ExecutorService
     private lateinit var analyzer: MyImageAnalyzer
+    private lateinit var buttonScanner: Button
+    private lateinit var cameraFragment: CameraFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
         checkCameraPermission();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        if (savedInstanceState == null) {
+            // 2
+            supportFragmentManager
+                // 3
+                .beginTransaction()
+
+                // 4|
+                .add(R.id.fragment_container_view, cameraFragment)
+                // 5
+                .commit()
+        }
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
             @Suppress("DEPRECATION")
@@ -49,10 +74,26 @@ class MainActivity : AppCompatActivity() {
         cameraProviderFuture.addListener(Runnable {
             val cameraProvider = cameraProviderFuture.get()
             bindPreview(cameraProvider)
-        }, ContextCompat.getMainExecutor(this))
+        }, ContextCompat.getMainExecutor(this))*/
     }
 
-    @SuppressLint("UnsafeExperimentalUsageError")
+    //init
+    private fun init() {
+        buttonScanner = findViewById(R.id.buttonScanner)
+        buttonScanner.setOnClickListener {
+            supportFragmentManager
+                // 3
+                .beginTransaction()
+
+                // 4|
+                .add(R.id.fragment_container_view, cameraFragment)
+                // 5
+                .commit()
+        }
+        cameraFragment = CameraFragment()
+    }
+
+   /* @SuppressLint("UnsafeExperimentalUsageError")
     private fun bindPreview(cameraProvider: ProcessCameraProvider) {
         val preview: Preview = Preview.Builder()
             .build()
@@ -73,7 +114,8 @@ class MainActivity : AppCompatActivity() {
             imageAnalysis,
             preview
         )
-    }
+    }*/
+
     // функция по правке прав на съемку
     private fun checkCameraPermission() {
         if (ContextCompat.checkSelfPermission(
@@ -89,4 +131,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+
 }
