cdd3e62794e858d51ddbc249cb9bf2ec582d050b
Vladislav Vedernikov <vladved2424@gmail.com>
2022-06-08 15:04:15 +0300

    add beacon fragment

---
+++ app/src/main/java/com/vladgad/qnb/BeaconFragment.kt
@@ -0,0 +1,12 @@
+package com.vladgad.qnb
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+
+class BeaconFragment : Fragment(R.layout.frag_beacon_receiver) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
