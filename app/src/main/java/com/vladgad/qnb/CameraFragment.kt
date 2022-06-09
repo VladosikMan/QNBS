b85ae0bcdcde420f0c11f17b72d8b1f8e015762d
Vladislav Vedernikov <vladved2424@gmail.com>
2022-06-09 14:07:43 +0300

    CameraFragment fix error

--- app/src/main/java/com/vladgad/qnb/CameraFragment.kt
+++ app/src/main/java/com/vladgad/qnb/CameraFragment.kt
@@ -1,7 +1,13 @@
 package com.vladgad.qnb
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.util.Size
 import android.view.View
 import androidx.camera.core.CameraSelector
@@ -20,8 +26,10 @@ class CameraFragment : Fragment(R.layout.frag_qr_scanner) {
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var cameraExecutor: ExecutorService
     private lateinit var analyzer: MyImageAnalyzer
+    private lateinit var imageAnalysis : ImageAnalysis
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        checkCameraPermission();
         analyzer = MyImageAnalyzer(getParentFragmentManager())
         cameraExecutor = Executors.newSingleThreadExecutor()
         cameraProviderFuture = activity?.let { ProcessCameraProvider.getInstance(it.applicationContext) } as ListenableFuture<ProcessCameraProvider>
@@ -42,7 +50,7 @@ class CameraFragment : Fragment(R.layout.frag_qr_scanner) {
             .build()
         preview.setSurfaceProvider(previewView.createSurfaceProvider(null))
 
-        val imageAnalysis = ImageAnalysis.Builder()
+        imageAnalysis= ImageAnalysis.Builder()
             .setTargetResolution(Size(1280, 720))
             .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
             .build()
@@ -54,5 +62,30 @@ class CameraFragment : Fragment(R.layout.frag_qr_scanner) {
             imageAnalysis,
             preview
         )
+    }
+
+    // функция по правке прав на съемку
+    private fun checkCameraPermission() {
+        if (activity?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.CAMERA
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
+            Intent().also {
+                it.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                it.data = Uri.fromParts("package", activity?.packageName, null)
+                startActivity(it)
+                activity?.finish()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        Log.d("mTag","Кукла")
+        cameraProviderFuture.get().unbindAll()
+
+        super.onDestroyView()
     }
 }
